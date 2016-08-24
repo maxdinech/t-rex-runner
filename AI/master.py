18a1a8f070fac017fd74cbc66a0b0563a8b55f37
Max de D <m.dinechin@yahoo.com>
2016-08-24 16:45:03 +0200

    import master in other AIs

--- AI/master.py
+++ AI/master.py
@@ -1,61 +1,38 @@
+# coding: utf8
+
 import pyautogui as pg
 import time, os, logging, sys, random, copy
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Opera()
-driver.get("http://localhost/t-rex-runner/")
+driver = webdriver.Chrome()
 
 
-
-# Global vars
-SPEED = 0
-OBS_DIST = 0
-OBS_SIZE = 0
-PASSED = 0
+def jump():
+    pg.press('space')
 
 
-def saut(t = 0):
-	pg.press('space')
+def getVars():
+    cookies = driver.get_cookies()
+    for i in range(4):
+        cookie = cookies[i]
+        if cookie['name'] == u'speed':
+            speed = float(cookie['value'])
+        if cookie['name'] == u'obs_dist':
+            obs_dist = int(cookie['value'])
+        if cookie['name'] == u'obs_size':
+            obs_size = int(cookie['value'])
+        if cookie['name'] == u'passed':
+            passed = int(cookie['value'])
+    return speed, obs_dist, obs_size, passed
 
 
-def botIdiot(n = 1000):
-	pg.click(350, 300)
-	for i in range(n):
-		saut(100)
-
-
-def reloadCookies():
-	global SPEED, OBS_DIST, OBS_SIZE, PASSED
-	cookies = driver.get_cookies()
-	for i in range(4):
-		cookie = cookies[i]
-		if cookie['name'] == u'speed':
-			SPEED = float(cookie['value'])
-		if cookie['name'] == u'obs_dist':
-			OBS_DIST = int(cookie['value'])
-		if cookie['name'] == u'obs_size':
-			OBS_SIZE = int(cookie['value'])
-		if cookie['name'] == u'passed':
-			PASSED = int(cookie['value'])
-	return
-	
-
-def main():
-	print('Press Ctrl-C to quit.')
-	try:
-	    while True:
-	    	reloadCookies()
-	        varsStr = 'SPEED: ' + str(SPEED).rjust(5) + '  -  OBS DIST: ' + str(OBS_DIST).rjust(3) \
-	        			+ '  -  OBS SIZE: ' + str(OBS_SIZE).rjust(2)
-	        print varsStr,
-	        print '\b' * (len(varsStr) + 2),
-	        sys.stdout.flush()
-	        if OBS_DIST < 170 and OBS_DIST > 50:
-	        	saut()
-	except KeyboardInterrupt:
-	    print '\n'
-
-
-main()
+def printVars(speed, obs_dist, obs_size, passed):
+    dispStr = ""
+    dispStr += '   ║ SPEED: ' + str(speed).rjust(5)
+    dispStr += '  -  OBS DIST: ' + str(obs_dist).rjust(3)
+    dispStr += '  -  OBS SIZE: ' + str(obs_size).rjust(2)
+    print dispStr,
+    print '\b' * (len(dispStr) + 2),
+    sys.stdout.flush()
--- AI/naive.py
+++ AI/naive.py
@@ -1,5 +1,31 @@
-import pyautogui as pg
-import time, os, logging, sys, random, copy
+# coding: utf8
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from master import *
+
+
+def naive_IA(jump_dist):
+    driver.get('http://localhost/t-rex-runner/')
+    print('\n')
+    print('    ╔════════════════════════════════════╗')
+    print('    ║T-Rex:        NAIVE IA          v1.0║')
+    print('   ╔╩════════════════════════════════════╩╗')
+    print('   ║ Jump trigger dist: ' + str(jump_dist) + ' px            ║')
+    print('   ║                 Press Ctrl-C to quit.║')
+    print('   ╚╦════════════════════════════════════╦╝')
+    try:
+        while True:
+            speed, obs_dist, obs_size, passed = getVars()
+            if obs_dist < jump_dist:
+                jump()
+            dispStr = ""
+            dispStr += '   ║ SPEED: ' + str(1000*speed).rjust(5)
+            dispStr += '  DIST: ' + str(obs_dist).rjust(3)
+            dispStr += '  SIZE: ' + str(obs_size).rjust(2) + " ║"
+            print dispStr,
+            print '\b' * (len(dispStr) + 2),
+            sys.stdout.flush()
+    except KeyboardInterrupt:
+        print '\n'
+
+
+naive_IA(170)
